e906d4c0d6c0d52430d66f9b8a675d4e7fcda5b0
Fernando Simões Pernabel <f.pernabel@influency.me>
2023-01-28 18:08:38 -0300

    feat: Criado versão inicial do projeto com o template do MAUI

---
+++ src/OmegaFY.Blog.Maui.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace OmegaFY.Blog.Maui.App;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
