b2836e650f7eded9ea55b261e234c28e9ae8b22f
Fernando Simões Pernabel <f.pernabel@influency.me>
2023-02-09 10:57:18 -0300

    feat (infra): Implementado classes de Localização (Recursos, MarkupExtension e Manager)

---
+++ src/OmegaFY.Blog.Maui.App/Infra/Localization/LocalizationResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using OmegaFY.Blog.Maui.App.Resources.Localization;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace OmegaFY.Blog.Maui.App.Infra.Localization;
+
+public class LocalizationResourceManager : INotifyPropertyChanged
+{
+    public static LocalizationResourceManager Instance { get; } = new();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private LocalizationResourceManager() => AppLocalizationResources.Culture = CultureInfo.CurrentCulture;
+
+    public void SetCulture(CultureInfo culture)
+    {
+        AppLocalizationResources.Culture = culture;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+    }
+
+    public object this[string resourceKey] => AppLocalizationResources.ResourceManager.GetObject(resourceKey, AppLocalizationResources.Culture);
+}
