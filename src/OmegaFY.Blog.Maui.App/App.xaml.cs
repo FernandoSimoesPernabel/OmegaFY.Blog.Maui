e09b59af6bec38e7eee1ccb915c86081cb804e4b
Fernando Simões Pernabel <f.pernabel@influency.me>
2024-01-11 12:15:15 -0300

    feat: Dimensionando a janela quando WinUI para um tamanho mais proximo de um celular

--- src/OmegaFY.Blog.Maui.App/App.xaml.cs
+++ src/OmegaFY.Blog.Maui.App/App.xaml.cs
@@ -8,4 +8,27 @@ public partial class App : Microsoft.Maui.Controls.Application
 
         MainPage = new AppShell();
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        if (DeviceInfo.Platform == DevicePlatform.WinUI)
+            window.Activated += Window_Activated;
+
+        return window;
+    }
+
+    private void Window_Activated(object sender, EventArgs e)
+    {
+        Window window = sender as Window;
+
+        window.Width = 360;
+        window.Height = 720;
+
+        DisplayInfo displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+
+        window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+        window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+    }
 }
